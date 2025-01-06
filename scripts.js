871a320fcf7167c997eb3702c07496c498e7255a
hone-productions <anthonysiderides14@gmail.com>
2025-01-06 15:14:14 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,20 +9,10 @@ function toggleMenu() {
 
 function handleScroll() {
   const sections = document.querySelectorAll('.section');
-  sections.forEach(section => {
+  sections.forEach((section) => {
     const rect = section.getBoundingClientRect();
     if (rect.top < window.innerHeight && rect.bottom > 0) {
       section.classList.add('visible');
-    }
-  });
-
-  const services = document.querySelectorAll('.service');
-  services.forEach((service, index) => {
-    const rect = service.getBoundingClientRect();
-    if (rect.top < window.innerHeight && rect.bottom > 0) {
-      setTimeout(() => {
-        service.classList.add('visible');
-      }, index * 200);
     }
   });
 }
